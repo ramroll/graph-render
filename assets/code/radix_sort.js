bb1745e389edf4c1bcafd049c3b2476b2891bd23
Ramroll Wei <ramrollwei@RamrolldeMacBook-Pro.local>
2018-09-04 22:28:39 +0800

    radix sort

--- assets/code/radix_sort.js
+++ assets/code/radix_sort.js
@@ -4,9 +4,21 @@ function radix_sort(A){
   let m = 1
   while(m < max) {
     const buckets = Array.from({ length: 10 }, () => [])
+
+    // 将所有数字写入桶中
     A.forEach( number => {
-      digit = number
+      const digit = ~~ ( ( number % (m * 10) ) / m )
+      console.log(`digit=${digit}`)
+      buckets[digit].push(number)
     })
+
+    // 将数组从桶中拿出
+    let j = 0
+    buckets.forEach(bucket => bucket.forEach(v => A[j++] = v))
     m *= 10
   }
 }
+
+const A = [10, 200,13,12, 7, 88,91, 24]
+radix_sort(A)
+console.log(A)
